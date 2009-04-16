f86bbd258c2d4913f4d3f5e7dd8984465057ec0f
hpoydar <hpoydar@gmail.com>
2009-04-16 17:19:38 -0400

    Updated base rails template with sprockets, jQuery, cucumber and more

--- ruby_on_rails/application_templates/base.rb
+++ ruby_on_rails/application_templates/base.rb
@@ -1,8 +1,14 @@
+##
+## See output at the end of this file for a list
+## of everything this template includes
+##
+
 # Gems
 gem 'haml'
 gem 'cucumber'
 gem 'rspec', :lib => false 
-gem 'rspec-rails', :lib => false 
+gem 'rspec-rails', :lib => false
+gem 'sprockets'
 gem 'webrat' 
 
 # Rspec generation
@@ -55,13 +61,24 @@ file 'app/views/layouts/application.html.haml', <<-CODE
     %title 
       TODO: Application title
     
-    = stylesheet_link_tag 'screen', :media => 'screen'
-    = stylesheet_link_tag 'print', :media => 'print'
+    = stylesheet_link_tag 'screen', :cache => 'all_screen', :media => 'screen'
+    = stylesheet_link_tag 'print', :cache => 'all_print', :media => 'print'
+    
+    = sprockets_include_tag
     
     :javascript
       authenticity_token = '\#{form_authenticity_token}';
   
   %body
+  
+    #container
+    
+      #header
+    
+      #main
+        = yield
+  
+      #footer
   
 CODE
 
@@ -92,12 +109,30 @@ class Time
   end
 end
 CODE
-
-# Site controller, specs, views
-file 'config/initializers/extensions.rb', <<-CODE
+initializer 'extensions.rb', <<-CODE
 require 'time_extension'
 CODE
+file 'spec/lib/time_extension_spec.rb', <<-CODE
+require File.dirname(__FILE__) + '/../spec_helper'
 
+describe Time, 'extensions' do
+  
+  before :each do
+    @time = Time.parse('2009-04-01 16:00:00')
+  end
+  
+  it 'should drop the leading 0 from the date when &d instead of %d is passed into strftime' do
+    @time.strftime('%b &d').should == 'Apr 1'
+  end
+  
+  it 'should drop the leading 0 from the hour when &I instead of %I is passed into strftime' do
+    @time.strftime('&I:%M %p').should == '4:00 PM'
+  end
+  
+end
+CODE
+
+# Site controller, specs, views
 file 'app/controllers/site_controller.rb', <<-CODE
 class SiteController < ApplicationController
   
@@ -105,13 +140,10 @@ class SiteController < ApplicationController
     
 end
 CODE
-
 run 'mkdir -p app/views/site'
 run 'touch app/views/site/index.html.haml'
-
 run 'mkdir -p spec/controllers'
 run 'mkdir -p spec/views/site'
-
 file 'spec/controllers/site_controller_spec.rb', <<-CODE
 require File.dirname(__FILE__) + '/../spec_helper'
 
@@ -140,8 +172,7 @@ describe SiteController do
   
 end
 CODE
-
-file 'spec/views/site/index.html.haml_spec', <<-CODE
+file 'spec/views/site/index.html.haml_spec.rb', <<-CODE
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/site/index.html.haml" do
@@ -156,11 +187,35 @@ describe "/site/index.html.haml" do
 end
 CODE
 
+# Initial features
+file 'features/home_page.feature', <<-CODE
+Feature: Home Page
+  As a web user
+  In order to use this site
+  I want to visit the homepage
+  Scenario: view homepage
+    Given I am an outside user
+    When I go to the homepage
+    Then I should see the homepage
+CODE
+file 'features/step_definitions/home_page_steps.rb', <<-CODE
+Given /^I am an outside user$/ do; end
+
+When /^When I go to the homepage$/ do 
+  visit '/'
+end
+
+Then /^I should see the homepage$/ do
+  response.should be_success
+end
+CODE
+
 # Remove default routes and clean up the routes file
 file 'config/routes.rb', <<-CODE
 ActionController::Routing::Routes.draw do |map|
   
   map.root :controller => 'site'
+  SprocketsApplication.routes(map) 
   
 end
 CODE
@@ -176,6 +231,27 @@ run "rm public/images/rails.png"
 
 # Setup README'
 run 'touch doc/README.md'
+
+# Setup javascripts  (jQuery, sprockets)
+run 'script/plugin install git://github.com/sstephenson/sprockets-rails.git'
+run 'rm -rf public/javascripts/*.js'
+file 'config/sprockets.yml', <<-CODE
+:asset_root: public
+:load_path:
+  - app/javascripts
+  - vendor/sprockets/*/src
+  - vendor/plugins/*/javascripts
+:source_files:
+  - app/javascripts/jquery-*.js
+  - app/javascripts/jquery.*.js
+  - app/javascripts/**/*.js
+  - app/javascripts/application.js
+CODE
+jquery_file_name = 'jquery-1.3.2.min.js'
+run "curl -L http://jqueryjs.googlecode.com/files/#{jquery_file_name} > app/javascripts/#{jquery_file_name}"
+file 'app/javascripts/application.js', <<-CODE
+$(document).ready(function() {});
+CODE
 
 # Initialize git repository
 git :init
@@ -195,5 +271,41 @@ run 'touch tmp/.gitignore log/.gitignore vendor/.gitignore db/.gitignore'
 plugin 'rspec-on-rails-matchers', :git => 'git://github.com/joshknowles/rspec-on-rails-matchers.git', :submodule => true
 plugin 'ya-rspec-scaffolder', :git => 'git://github.com/hpoydar/ya-rspec-scaffolder.git', :submodule => true
 
-# Commit
+# Get submodules
+git :submodule => 'init'
+git :submodule => 'update'
+
+# Initial commit
 git :add => ".", :commit => "-m 'initial commit'"
+
+# Run initial specs and features
+puts 'Setting up schema ...'
+run 'rake db:migrate'
+puts 'Running initial specs ...'
+run 'rake spec'
+puts 'Stepping through initial features ...'
+run 'rake features'
+
+puts ''
+puts 'Application setup with:'
+puts '* git SCM'
+puts '* Rspec BDD framework'
+puts '* Cucumber BDD framework'
+puts '* Haml/Sass formatting library'
+puts '* rspec-on-rails-matchers plugin (git submodule)'
+puts '* ya-rspec-scaffolder plugin (git submodule)'
+puts '* sprockets-rails plugin (git submodule)'
+puts '* jQuery (in place of default prototype.js)'
+puts '* sprockets.yml setup for jQuery'
+puts '* DB-in-memory test environment'
+puts '* README documentation converted to markdown'
+puts '* A Time class extension for dropping leading zeros'
+puts "* A general 'site' controller with an index action"
+puts '* A single root path to the site controller'
+puts '* A simple application layout in Haml'
+puts '* Initial controller and view specs'
+puts '* An initial Cucumber feature'
+puts ''
+
+
+
